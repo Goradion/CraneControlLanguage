3abd93ae3443b9730e7be8b45abb7cb462f6e193
Simon Bastian <SimonFBastian@googlemail.com>
2018-04-20 14:44:37 +0200

    init

---
+++ src/main/java/de/sbam/swks/CraneControlLanguage/CraneControlLanguageApplication.java
@@ -0,0 +1,12 @@
+package de.sbam.swks.CraneControlLanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraneControlLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraneControlLanguageApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sbam/swks/CraneControlLanguage/CraneControlLanguageApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sbam.swks.CraneControlLanguage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CraneControlLanguageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
