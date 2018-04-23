5f3b14e64f70f61e1c71459c5784c6dc2e162148
am <alexander_mueller1987@gmail.com>
2018-04-23 12:52:49 +0200

    simple helloworld controller and service test

---
+++ src/main/java/de/sbam/swks/CraneControlLanguage/HelloWorldService.java
@@ -0,0 +1,11 @@
+package de.sbam.swks.CraneControlLanguage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+	public String hello() {
+		return "Hello Worldddd!";
+	}
+}
